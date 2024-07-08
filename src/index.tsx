166a66921a4754fc1ae587754dab43faf045b3c5
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2024-07-08 16:04:13 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ArcGISMapComponent from './ArcGISMapComponent';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ArcGISMapComponent/>
   </React.StrictMode>
 );
 
